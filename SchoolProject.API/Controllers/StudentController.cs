693f490b646d15471927e8041669e88c4b17f6a9
Wael Orabi <w.orabi@opstudioz.com>
2024-12-26 11:41:51 +0300

    Handle End Response

--- SchoolProject.API/Controllers/StudentController.cs
+++ SchoolProject.API/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchoolProject.API.Base;
 using SchoolProject.Core.Features.Students.Commands.Models;
 using SchoolProject.Core.Features.Students.Queries.Models;
 using SchoolProject.Data.AppMetaData;
@@ -9,30 +10,26 @@ namespace SchoolProject.API.Controllers
 {
  //   [Route("api/[controller]")]
     [ApiController]
-    public class StudentController : ControllerBase
+    public class StudentController :AppControllerBase
     {
-        private readonly IMediator _mediator;
-        public StudentController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+      
         [HttpGet(Router.StudentRouting.List)]
         public async Task<IActionResult> GetStudentList()
         {
-            var response = await _mediator.Send(new GetStudentListQuery());
-            return Ok( response);
+            var response = await Mediator.Send(new GetStudentListQuery());
+            return NewResult( response);
         }
         [HttpGet(Router.StudentRouting.GetByID)]
         public async Task<IActionResult> GetStudentById(int id)
         {
-            var response = await _mediator.Send(new GetStudentByIdQuery(id));
-            return Ok(response);
+            var response = await Mediator.Send(new GetStudentByIdQuery(id));
+            return NewResult(response);
         }
         [HttpPost(Router.StudentRouting.Create)]
         public async Task<IActionResult> AddStudent([FromBody]AddStudentCommand addStudentCommand)
         {
-            var response = await _mediator.Send(addStudentCommand);
-            return Ok(response);
+            var response = await Mediator.Send(addStudentCommand);
+            return NewResult(response);
         }
     }
 }
